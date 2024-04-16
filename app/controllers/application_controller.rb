85362bd1c2501e2f1b6aeab9feb523e8e1810da8
simiedeki <164128583+simiedeki@users.noreply.github.com>
2024-04-16 20:59:09 +0000

    draft

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,43 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
+
+    def home
+    end
+  
+    def rock
+      player_move = ["rock", "paper", "scissors"].sample
+      @yourthrow = "We played rock!"
+      @theirthrow = "They played #{player_move}."
+      @outcome = evaluate_game("rock", player_move)
+    end
+  
+    def paper
+      player_move = ["rock", "paper", "scissors"].sample
+      @yourthrow = "We played paper!"
+      @theirthrow = "They played #{player_move}."
+      @outcome = evaluate_game("paper", player_move)
+    end
+  
+    def scissors
+      player_move = ["rock", "paper", "scissors"].sample
+      @yourthrow = "We played scissors!"
+      @theirthrow = "They played #{player_move}."
+      @outcome = evaluate_game("scissors", player_move)
+    end
+  
+    private
+  
+    def evaluate_game(user_move, player_move)
+      if user_move == player_move
+        "tied"
+      elsif (user_move == "rock" && player_move == "scissors") ||
+            (user_move == "scissors" && player_move == "paper") ||
+            (user_move == "paper" && player_move == "rock")
+        "won"
+      else
+        "lost"
+      end
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
 
+    root 'game#home'
+    get '/home', to: redirect('/')
+    get '/rock', to: 'game#rock'
+    get '/paper', to: 'game#paper'
+    get '/scissors', to: 'game#scissors'
+  
 end
